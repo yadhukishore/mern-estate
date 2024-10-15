0b0f0fa4cd70d3a7e0db79d8828fb726eaa7e040
Yadhukrishna <yadhukishore37@gmail.com>
2024-10-15 16:15:44 +0530

    Connected to DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGOURL).then(()=>{
+    console.log("Connected to MongoDB")
+}).catch((err)=>{
+    console.log("Error In Mongo Connect: ",err);
+})
 const app = express();
 app.listen(4000,()=>{
     console.log(`Server is running on 4000`);
