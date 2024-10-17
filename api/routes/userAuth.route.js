a9dcc0fd39f33de8a67eb3482b52c60209766eae
Yadhukrishna <yadhukishore37@gmail.com>
2024-10-17 14:40:21 +0530

    create sign in api routes and jwt

--- api/routes/userAuth.route.js
+++ api/routes/userAuth.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { signup } from '../controllers/userAuth.controller.js';
+import { signin, signup } from '../controllers/userAuth.controller.js';
 const router = express();
 router.post('/signup',signup);
+router.post('/signin',signin);
 
 export default router;
