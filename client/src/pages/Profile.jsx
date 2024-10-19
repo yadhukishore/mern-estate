ff4337d96da989143e2e7b540136add6438679b1
Yadhukrishna <yadhukishore37@gmail.com>
2024-10-19 16:06:13 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,24 +1,79 @@
-import { useSelector } from "react-redux"
-
+import { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { app } from "../firebase/firebase";
 
 const Profile = () => {
-  const {currentUser} = useSelector((state)=> state.user);
+  const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePercentage, setFilePercentage] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePercentage(Math.round(progress));
+      },
+      () => {
+        setFileUploadError(true); 
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) =>
+          setFormData({ ...formData, avathar: downloadUrl })
+        );
+      }
+    );
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl font-bold text-center my-7 cursor-none" >My Profile</h1>
+      <h1 className="text-3xl font-bold text-center my-7 cursor-none">My Profile</h1>
       <form className="flex flex-col gap-4">
-        <img src={currentUser.avathar} alt="profile!" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
-        <input type="text" placeholder="username" id="username" className="border p-3 rounded-md"/>
-        <input type="email" placeholder="email" id="email" className="border p-3 rounded-md"/>
-        <input type="password" placeholder="password" id="password" className="border p-3 rounded-md"/>
-        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-85" >Update</button>
+        <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*" />
+        <img
+          onClick={() => fileRef.current.click()}
+          src={formData.avathar || currentUser.avathar}
+          alt="profile!"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
+        <p className="text-sm self-center">
+          {fileUploadError ? (
+            <span className="text-red-700">Error Image Upload!!</span>
+          ) : filePercentage > 0 && filePercentage < 100 ? (
+            <span className="text-slate-700">{`Uploading ${filePercentage}%`}</span>
+          ) : filePercentage === 100 ? (
+            <span className="text-green-700">Successfully Uploaded!!!</span>
+          ) : (
+            ""
+          )}
+        </p>
+
+        <input type="text" placeholder="username" id="username" className="border p-3 rounded-md" />
+        <input type="email" placeholder="email" id="email" className="border p-3 rounded-md" />
+        <input type="password" placeholder="password" id="password" className="border p-3 rounded-md" />
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-85">Update</button>
       </form>
-      <div className="flex justify-between mt-5" >
-          <span className="text-red-700 cursor-pointer" >Delete account</span>
-          <span className="text-red-700 cursor-pointer" >Sign Out?</span>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span className="text-red-700 cursor-pointer">Sign Out?</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
